ff6f1965a22800ffc078331c0e0f5692ebea1d7d
lazinferisl80 <isl.ivan.ivan@gmail.com>
2020-01-08 20:24:04 +0100

    ok

---
+++ app.js
@@ -0,0 +1,5 @@
+var concierto= ("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+var ciudades= ("madrid", "sevilla", "barcelona", "valencia","zaragoza", "bilbao", "donosti")
+
+alert= (concierto,split(","));
+alert= (ciudades,join(","));
